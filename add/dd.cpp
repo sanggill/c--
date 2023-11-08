40bc90d2f6f480387562c2b29319beb03bd8bd8e
sanggill <ysg960@gmaill.com>
2023-11-08 09:12:13 +0900

    first commit

---
+++ add/dd.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+using namespace std;
+
+class Plus{
+    public:
+    Plus(){
+        num = 0;
+    }
+    int operator()(){
+        num++;
+        return num;
+    }
+    int operator()(int n){
+        num = ++n;
+        return num;
+    }
+    friend ostream& operator<<(ostream&,const Plus&);
+
+    private:
+    int num;
+};
+
+ostream& operator<<(ostream& os , const Plus&plus){
+        os << plus.num<<endl;
+        return os;
+}
+
+int main(){
+    Plus plus;
+    int num;
+    num=plus(5);
+
+    cout<<num<<endl;
+    return 0;
+}
+
+
